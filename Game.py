6af11cbdb0e18bdd16081ec00ba816731449cf09
Us3r-Unknown <fabianosouza@hotmail.com>
2025-04-21 11:51:39 -0300

    jogo side scrolling usando pygame.

--- Game.py
+++ Game.py
@@ -1,17 +1,48 @@
 import pygame
 import sys
+from Scripts.Utils import load_image
+from Scripts.Entities import PhisicsEntity
+class Game:
+    def __init__(self):
+        pygame.init()
 
-pygame.init()
+        pygame.display.set_caption('Ninja Game')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+        self.clock = pygame.time.Clock()
 
-pygame.display.set_caption('Ninja Game')
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
+        self.movement = [False, False]
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
 
-    pygame.display.update()
-    clock.tick(60)
+        self.player = PhisicsEntity(self, 'player', (50, 50), (8, 15))
+
+    def run(self):
+        while True:
+            self.display.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
